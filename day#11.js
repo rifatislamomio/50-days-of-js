8e9161e32e74e883c0e07164779f80bcd13d5c8c
Rifat Islam <rifat.islam173@northsouth.edu>
2022-01-08 23:57:44 +0600

    day#13 added

--- day#11.js
+++ day#11.js
@@ -1,7 +1,9 @@
 //Return the N-th value of the Fibonacci sequence
 function fibonacci(n) {
 	if (n==1 || n==2) return 1;
-    else{
+    
+    else
+    {
         return fibonacci(n-1)+fibonacci(n-2);
     }
 }
---
+++ day#13.js
@@ -0,0 +1,22 @@
+//Convert given seconds to space age on all planets of our solar system
+const spaceAge = (seconds) => {
+	const yearsInAllPlanets = {
+		Mercury: 0.2408467,
+		Venus: 0.61519726,
+		Earth: 1.0,
+		Mars: 1.8808158,
+		Jupiter: 11.862615,
+		Saturn: 29.447498,
+		Uranus: 84.016846,
+		Neptune: 164.79132,
+	}
+	for(x of Object.keys(yearsInAllPlanets))
+    {
+        //yearsInAllPlanets[x] = (seconds / (yearsInAllPlanets[x]*365.25*24*60*60)).toFixed(2);
+        var value = (seconds / (yearsInAllPlanets[x]*365.25*24*60*60)).toFixed(2)
+        yearsInAllPlanets[x] = parseFloat(value) ;
+    }
+	return yearsInAllPlanets
+}
+
+console.log(spaceAge(Math.round(Math.random() * 99999999)))
